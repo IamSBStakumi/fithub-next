bb7a6e33319ff3c000e4e2c8ac1a98d21504d550
IamSBStakumi <kuribayashi.takumi0608@gmail.com>
2025-12-11 15:21:58 +0900

    implement signin

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,21 @@
 "use client";
 
+import { createClient } from "@/lib/supabase/client";
+
 const Login = () => {
-  const handleLogin = async () => {};
+  const handleLogin = async () => {
+    const supabase = createClient();
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: "http://localhost:3000/auth/callback",
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+  };
 
   return (
     <div>

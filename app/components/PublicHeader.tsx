ea3884bc03eecdc056f5c047eeb54c2ddde874b2
kuri884 <krc.kuribayashi@gmail.com>
2025-12-18 16:02:30 +0900

    implement go to signin button

--- app/components/PublicHeader.tsx
+++ app/components/PublicHeader.tsx
@@ -1,7 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 const PublicHeader = () => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push("/login");
+  };
+
   return (
     <header>
       <h1>Public Header</h1>
+      <button onClick={handleClick}>サインイン</button>
     </header>
   );
 };
